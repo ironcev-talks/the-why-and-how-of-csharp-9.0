8d3ee9b4282cc4e205c3a2a69890360aa0f53039
Igor Rončević <ironcev@hotmail.com>
2021-01-22 23:54:19 +0100

    Add InitOnlySetters.AspNetCore31 project

---
+++ examples/InitOnlySetters/InitOnlySetters.AspNetCore31/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace InitOnlySetters.AspNetCore31
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; init; }
+
+        public int TemperatureC { get; init; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; init; }
+    }
+}
