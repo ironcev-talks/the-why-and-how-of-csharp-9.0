d109a1e277efc572489a9d30c31b7edc12169ada
Igor Rončević <ironcev@hotmail.com>
2021-01-23 05:32:23 +0100

    Add TargetTypedNewExpressions.AllInOneExample project

---
+++ examples/TargetTypedNewExpressions/TargetTypedNewExpressions.AllInOneExample/After.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TargetTypedNewExpressions.AllInOneExample
+{
+    public class After
+    {
+        private readonly Dictionary<string, string> dictionary = new ();
+
+        protected List<After> Children { get; } = new ();
+
+        public IEnumerable<DateTime> GetDates()
+        {
+            IEnumerable<DateTime> dates = new DateTime[]
+            {
+                new (2021, 01, 23),
+                new (2021, 02, 24),
+                new (2021, 03, 25),
+                new (2021, 04, 26),
+                new (2021, 05, 27)
+            };
+
+            SomeHelperLocalFunction(new (1999, 12, 13, 12, 0, 0));
+
+            return dates;
+
+            void SomeHelperLocalFunction(DateTime date)
+            {
+            }
+        }
+    }
+}
---
+++ examples/TargetTypedNewExpressions/TargetTypedNewExpressions.AllInOneExample/Before.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TargetTypedNewExpressions.AllInOneExample
+{
+    public class Before
+    {
+        private readonly Dictionary<string, string> dictionary = new Dictionary<string, string>();
+
+        protected List<Before> Children { get; } = new List<Before>();
+
+        public IEnumerable<DateTime> GetDates()
+        {
+            IEnumerable<DateTime> dates = new DateTime[]
+            {
+                new DateTime(2021, 01, 23),
+                new DateTime(2021, 02, 24),
+                new DateTime(2021, 03, 25),
+                new DateTime(2021, 04, 26),
+                new DateTime(2021, 05, 27)
+            };
+
+            SomeHelperLocalFunction(new DateTime(1999, 12, 13, 12, 0, 0));
+
+            return dates;
+
+            void SomeHelperLocalFunction(DateTime date)
+            {
+            }
+        }
+    }
+}
