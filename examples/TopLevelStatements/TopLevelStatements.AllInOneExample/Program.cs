2a72e7fb1ef14de808c38f96c20f6592cc93a14b
Igor Rončević <ironcev@hotmail.com>
2021-01-23 05:14:40 +0100

    Add examples for Top level statements

---
+++ examples/TopLevelStatements/TopLevelStatements.AllInOneExample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using static System.Console;
+
+WriteLine("Hello World!");
+
+if (args.Length > 0)
+{
+    WriteLine("Args are provided.");
+}
+else
+{
+    WriteLine("Args are not provided.");
+}
+
+await Task.Delay(100);
+
+UseSomeUsefulData(new SomeUsefulData());
+
+var someObject = new SomeNamespace.SomeClass();
+
+return 0;
+
+void UseSomeUsefulData(SomeUsefulData someUsefulData)
+{
+}
+
+record SomeUsefulData();
+
+namespace SomeNamespace
+{
+    class SomeClass { }
+}
---
+++ examples/TopLevelStatements/TopLevelStatements.OldHelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TopLevelStatements.OldHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
